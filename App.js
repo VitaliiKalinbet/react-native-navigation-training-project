713cca06ee6d2d2ff999b29cff4f7770a0117cc8
Vitalii Kalinbet <vitalii.kalinbet@patrianna.com>
2025-12-17 15:00:04 +0100

    init project

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
