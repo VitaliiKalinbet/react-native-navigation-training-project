fa5ffe87d7c48c6d3106acdfc63bb62c4a515cd6
Vitalii Kalinbet <vitalii.kalinbet@patrianna.com>
2025-12-17 17:04:27 +0100

    add base navigation packages

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
